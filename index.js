a1a2ef82775579057af584f9f80f04d84a3059b9
Abdul-Gafur <abdul.taramov2001@gmail.com>
2022-03-23 19:29:04 +0300

    snacke case

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import camelCase from 'camelcase';
+import { snakeCase } from 'snake-case';
 
 const toCamelCase = (data) => {
   const objectToCamelCase = (obj) => {
@@ -18,4 +19,24 @@ const toCamelCase = (data) => {
   return objectToCamelCase(data);
 };
 
+const toSnakeCase = (data) => {
+  const objectToSnakeCase = (obj) => {
+    if (typeof obj !== 'object') return obj;
+    const entries = Object.entries(obj).map(([key, value]) => {
+      if (value && typeof value === 'object') {
+        return [snakeCase(key), toSnakeCase(value)];
+      }
+      return [snakeCase(key), value];
+    });
+    return Object.fromEntries(entries);
+  };
+
+  if (Array.isArray(data)) {
+    return data.map((item) => objectToSnakeCase(item));
+  }
+  return objectToSnakeCase(data);
+};
+
 export default toCamelCase;
+
+export { toSnakeCase, toCamelCase };
